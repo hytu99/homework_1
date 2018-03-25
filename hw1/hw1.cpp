c05b6d2b97741bc263efecae1d8c933aba2c0be6
hytu <1943211876@qq.com>
2018-03-25 18:23:36 +0800

    revise CountQuantity function

--- hw1/hw1.cpp
+++ hw1/hw1.cpp
@@ -6,21 +6,23 @@
 #include<string>
 using namespace std;
 
-#define WORD_MAX_LENTH 1024
+#define WORD_MAX_LENGTH 250
 #define isLetter(c) (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
 #define isNumber(c) (c >= '0' && c <= '9')
+#define HASHMOD 21248619
 
 int characterNum = 0;
 int wordNum = 0;
 int lineNum = 0;
 typedef struct Node 
 {
-	char word[WORD_MAX_LENTH];
+	char word[WORD_MAX_LENGTH];
+	char wordPreLow[WORD_MAX_LENGTH];//word excluding numbers in the end
 	int time;
 	struct Node *next;
-	int length;
-	int lenWithoutLastNum;
-}wordNode;
+}wordNode, *wordList;
+
+wordList headList[HASHMOD];
 
 int isLetterOrNumber(char ch)
 {
@@ -30,53 +32,125 @@ int isLetterOrNumber(char ch)
 		return 0;
 }
 
-void Count(char *fileName) 
+void Mystrncpy_lwr(char* &s, char *t, int k)
+{
+
+	int i;
+	for(i = 0; i < k; i++)
+	{	
+		s[i] = t[i];
+		if (s[i] >= 'A' && s[i] <= 'Z')
+			s[i] += 32;
+	}
+	s[i] = '\0';
+}
+
+int ELFhash(char* key) 
+{
+	unsigned long h = 0, g;
+	while (*key)
+	{
+		h = (h << 4) + *key++;
+		g = h & 0xF0000000L;
+		if (g)
+			h ^= g >> 24;
+		h &= ~g;
+	}
+	return(h % HASHMOD);
+}
+
+void CountFrequency(char word[], int length, int lengthPre) 
+{
+	char* wordPreLow = new char[lengthPre + 1];
+	int h;
+	wordList wNode,p,q = NULL;
+
+	Mystrncpy_lwr(wordPreLow, word, lengthPre);
+
+	h = ELFhash(wordPreLow);
+	if(headList[h] == NULL)
+	{
+		wNode = new wordNode;
+		wNode->time = 1;
+		strcpy(wNode->word, word);
+		strcpy(wNode->wordPreLow, wordPreLow);
+		wNode->next = NULL;
+		headList[h] = wNode;
+		headList[h]->next = NULL;
+	}
+	else
+	{
+		p = headList[h];
+		while (p != NULL)
+		{
+			if (strcmp(p->wordPreLow, wordPreLow) == 0)
+			{
+				p->time++;
+				if (strcmp(word, p->word) < 0)
+				{
+					strcpy(p->word, word);
+				}
+				break;
+			}
+			q = p;
+			p = p->next;
+		}
+		if (p == NULL)
+		{
+			wNode = new wordNode;
+			wNode->time = 1;
+			strcpy(wNode->word, word);
+			strcpy(wNode->wordPreLow, wordPreLow);
+			wNode->next = NULL;
+			q->next = wNode;
+		}
+	}
+}
+
+void CountQuantity(const char* fileName) 
 {
 	FILE *fp = NULL;
 	char ch;
 	char lastChar;
-	char tempWord[WORD_MAX_LENTH];//for preserve word temprorily
-	wordNode wNode;
+	char tempWord[WORD_MAX_LENGTH];//for preserving word temprorily
 	int i = 0, j = 0, firstFlag = 1;
 
 	if ((fp = fopen(fileName, "r")) == NULL) {
-		printf("cannot open this file .%s\n", fileName);
+		printf("cannot open this file %s\n", fileName);
 		getchar();
 		exit(0);
 	}
 	
 	while ((ch = fgetc(fp)) != EOF)
 	{	
-		if(ch >= 0 && ch <= 127)
+		if(ch > 0 && ch <= 127 && ch != '\n')
 			characterNum++;
 		if (ch == '\n') 
 		{
 			lineNum++;
 			lastChar = ch;//Preserve last character
 		}
-		if (firstFlag == 1 ||!((isLetter(ch) || isNumber(ch))))
-		{	
-			//the first character  or   not letter or number
-			firstFlag = 0;
-			if (isLetter(ch) || isNumber(ch)) 
-			{	
-				tempWord[i++] = ch;
-			}
-
-			while ((ch = fgetc(fp)) && (isLetter(ch) || isNumber(ch) ))
+		if (isLetter(ch) || isNumber(ch))
+		{
+			tempWord[i++] = ch;
+			while ((ch = fgetc(fp)) && (isLetter(ch) || isNumber(ch)))
 			{
 				characterNum++;
 				tempWord[i++] = ch;
-			}	
+			}
 			tempWord[i] = '\0';
 
 			if (i >= 4 && isLetter(tempWord[0]) && isLetter(tempWord[1]) && isLetter(tempWord[2]) && isLetter(tempWord[3]))
 			{
 				// it's a word
 				wordNum++;
+				for (j = i - 1; isNumber(tempWord[j]); j--)
+				{
+				}
+				CountFrequency(tempWord, i, j + 1);
 			}
 
-			if (ch >= 0 && ch <= 127)
+			if (ch > 0 && ch <= 127 && ch != '\n')
 				characterNum++;
 			if (ch == '\n')
 			{
@@ -85,8 +159,11 @@ void Count(char *fileName)
 			}
 
 			i = 0;
-
 		}
+		else
+		{
+			continue;
+		}	
 	}
 
 	fclose(fp);
@@ -130,7 +207,10 @@ void TraverseFolder(string folderPath)
 		}
 		else
 		{
-			printf("%s\n", fileInfo.name);
+			//printf("%s\n", fileInfo.name);
+			string fullName;
+			fullName = folderPath + "\\" + fileInfo.name;
+			CountQuantity(fullName.c_str());
 		}
 	} while (_findnext(Handle, &fileInfo) == 0);
 
@@ -140,9 +220,12 @@ void TraverseFolder(string folderPath)
 
 int main(int argc, char *argv[]) 
 {
-
+	int i;
+	wordNode *p;
 	/*_finddata_t fileInfo;
 	long Handle = _findfirst(argv[1], &fileInfo);
+	int i;
+
 	if (Handle != -1L && (fileInfo.attrib & _A_SUBDIR) == 0)
 	{
 		//It's a file name and not a folder path 
@@ -154,8 +237,28 @@ int main(int argc, char *argv[])
 		TraverseFolder(argv[1]);
 	}*/
 	char s[] = "D:\\test\\123.txt";
-	Count(s);
-	printf("%d %d %d\n", characterNum, lineNum, wordNum);
+	CountQuantity(s);
+
+	//TraverseFolder(s);
+	
+	printf("characters: %d\n", characterNum);
+	printf("words: %d\n", wordNum);
+	printf("lines: %d\n", lineNum);
+	
+	/*for (i = 0; i < HASHMOD; i++)
+	{
+		if (headList[i] != NULL)
+		{	
+			p = headList[i];
+			while (p != NULL) 
+			{	
+				if(p->time >= 80000)
+					printf("%s :%d\n", headList[i]->word, headList[i]->time);
+				p = p->next;
+			}
+		}
+	}*/
+
 	getchar();
 	return 0;
 }
