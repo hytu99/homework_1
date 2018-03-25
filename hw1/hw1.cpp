4a655c1aff504eae126ac565dcb295da17a71acb
hytu <1943211876@qq.com>
2018-03-26 00:45:24 +0800

    Words/Phrases Frequency

--- hw1/hw1.cpp
+++ hw1/hw1.cpp
@@ -14,15 +14,26 @@ using namespace std;
 int characterNum = 0;
 int wordNum = 0;
 int lineNum = 0;
-typedef struct Node 
+typedef struct wordNode 
 {
 	char word[WORD_MAX_LENGTH];
 	char wordPreLow[WORD_MAX_LENGTH];//word excluding numbers in the end
 	int time;
-	struct Node *next;
+	struct wordNode *next;
 }wordNode, *wordList;
 
+typedef struct phraseNode
+{
+	int time;
+	struct phraseNode *next;
+	wordList word1;
+	wordList word2;
+}phraseNode, *phraseList;
+
 wordList headList[HASHMOD];
+phraseList headList_Phrase[HASHMOD];
+wordNode wordTop10[11];
+phraseNode phraseTop10[11];
 
 int isLetterOrNumber(char ch)
 {
@@ -59,15 +70,38 @@ int ELFhash(char* key)
 	return(h % HASHMOD);
 }
 
-void CountFrequency(char word[], int length, int lengthPre) 
+int ELFhash_Phrase(char* key1, char* key2)
+{
+	unsigned long h = 0, g;
+	while (*key1)
+	{
+		h = (h << 4) + *key1++;
+		g = h & 0xF0000000L;
+		if (g)
+			h ^= g >> 24;
+		h &= ~g;
+	}
+	while (*key2)
+	{
+		h = (h << 4) + *key2++;
+		g = h & 0xF0000000L;
+		if (g)
+			h ^= g >> 24;
+		h &= ~g;
+	}
+	return(h % HASHMOD);
+}
+
+wordList CountFrequency(char word[], int lengthPre) 
 {
 	char* wordPreLow = new char[lengthPre + 1];
 	int h;
-	wordList wNode,p,q = NULL;
+	wordList wNode = NULL, p, q = NULL;
 
 	Mystrncpy_lwr(wordPreLow, word, lengthPre);
 
 	h = ELFhash(wordPreLow);
+
 	if(headList[h] == NULL)
 	{
 		wNode = new wordNode;
@@ -90,7 +124,7 @@ void CountFrequency(char word[], int length, int lengthPre)
 				{
 					strcpy(p->word, word);
 				}
-				break;
+				return p;
 			}
 			q = p;
 			p = p->next;
@@ -105,6 +139,48 @@ void CountFrequency(char word[], int length, int lengthPre)
 			q->next = wNode;
 		}
 	}
+	return wNode;
+}
+
+void CountFrequency_Phrase(wordList word1, wordList word2) {
+
+	int h;
+	phraseList pNode, p, q = NULL;
+	h = ELFhash_Phrase(word1->wordPreLow, word2->wordPreLow);
+
+	if (headList_Phrase[h] == NULL)
+	{
+		pNode = new phraseNode;
+		pNode->time = 1;
+		pNode->word1 = word1;
+		pNode->word2 = word2;
+		headList_Phrase[h] = pNode;
+		headList_Phrase[h]->next = NULL;
+
+	}
+	else
+	{
+		p = headList_Phrase[h];
+		while (p != NULL)
+		{
+			if (strcmp(p->word1->wordPreLow, word1->wordPreLow) == 0 && strcmp(p->word2->wordPreLow, word2->wordPreLow) == 0)
+			{
+				p->time++;
+				break;
+			}
+			q = p;
+			p = p->next;
+		}
+		if (p == NULL)
+		{
+			pNode = new phraseNode;
+			pNode->time = 1;
+			pNode->word1 = word1;
+			pNode->word2 = word2;
+			pNode->next = NULL;
+			q->next = pNode;
+		}
+	}
 }
 
 void CountQuantity(const char* fileName) 
@@ -113,7 +189,8 @@ void CountQuantity(const char* fileName)
 	char ch;
 	char lastChar;
 	char tempWord[WORD_MAX_LENGTH];//for preserving word temprorily
-	int i = 0, j = 0, firstFlag = 1;
+	wordList wNode, wNodePre = NULL;
+	int i = 0, j = 0, flag = 0;
 
 	if ((fp = fopen(fileName, "r")) == NULL) {
 		printf("cannot open this file %s\n", fileName);
@@ -133,7 +210,7 @@ void CountQuantity(const char* fileName)
 		if (isLetter(ch) || isNumber(ch))
 		{
 			tempWord[i++] = ch;
-			while ((ch = fgetc(fp)) && (isLetter(ch) || isNumber(ch)))
+			while ((ch = fgetc(fp)) != EOF && (isLetter(ch) || isNumber(ch)))
 			{
 				characterNum++;
 				tempWord[i++] = ch;
@@ -147,7 +224,15 @@ void CountQuantity(const char* fileName)
 				for (j = i - 1; isNumber(tempWord[j]); j--)
 				{
 				}
-				CountFrequency(tempWord, i, j + 1);
+				wNode = CountFrequency(tempWord, j + 1);
+				if (flag == 1)
+				{
+					CountFrequency_Phrase(wNodePre,wNode);
+				}
+				//strcpy(wNodePre->word, wNode->word);
+				//strcpy(wNodePre->wordPreLow, wNode->wordPreLow);
+				wNodePre = wNode;
+				flag = 1;
 			}
 
 			if (ch > 0 && ch <= 127 && ch != '\n')
@@ -220,24 +305,24 @@ void TraverseFolder(string folderPath)
 
 int main(int argc, char *argv[]) 
 {
-	int i;
+	int i, j;
 	wordNode *p;
-	/*_finddata_t fileInfo;
+	phraseNode *q;
+	_finddata_t fileInfo;
 	long Handle = _findfirst(argv[1], &fileInfo);
-	int i;
 
 	if (Handle != -1L && (fileInfo.attrib & _A_SUBDIR) == 0)
 	{
 		//It's a file name and not a folder path 
-		printf("%s\n", fileInfo.name);
+		CountQuantity(argv[1]);
 	}
 	else
 	{	
 		//it's a folder path
 		TraverseFolder(argv[1]);
-	}*/
-	char s[] = "D:\\test\\123.txt";
-	CountQuantity(s);
+	}
+	//char s[] = "D:\\test\\newsample";
+	//CountQuantity(s);
 
 	//TraverseFolder(s);
 	
@@ -245,19 +330,66 @@ int main(int argc, char *argv[])
 	printf("words: %d\n", wordNum);
 	printf("lines: %d\n", lineNum);
 	
-	/*for (i = 0; i < HASHMOD; i++)
+	for (i = 0; i < HASHMOD; i++)
 	{
+		//find top 10 words
 		if (headList[i] != NULL)
 		{	
 			p = headList[i];
 			while (p != NULL) 
 			{	
-				if(p->time >= 80000)
-					printf("%s :%d\n", headList[i]->word, headList[i]->time);
+				//if(p->time >= 80000)
+				//	printf("%s: %d\n", headList[i]->word, headList[i]->time);
+				for (j = 9; j >= 0 && p->time > wordTop10[j].time || ((p->time == wordTop10[j].time) && (strcmp(p->word, wordTop10[j].word) < 0)); j--)
+				{
+					strcpy(wordTop10[j + 1].word, wordTop10[j].word);
+					wordTop10[j + 1].time = wordTop10[j].time;
+				}
+				if (j < 9) 
+				{
+					strcpy(wordTop10[j + 1].word, p->word);
+					wordTop10[j + 1].time = p->time;
+				}
 				p = p->next;
 			}
 		}
-	}*/
+	}
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%s: %d\n", wordTop10[i].word, wordTop10[i].time);
+	}
+
+	for (i = 0; i < HASHMOD; i++)
+	{
+		//find top 10 phrases
+		
+		if (headList_Phrase[i] != NULL)
+		{
+			q = headList_Phrase[i];
+			while (q != NULL)
+			{
+				for (j = 9; j >= 0 && q->time > phraseTop10[j].time || ((q->time == phraseTop10[j].time) && ((strcmp(q->word1->word, phraseTop10[j].word1->word) < 0)|| (strcmp(q->word2->word, phraseTop10[j].word2->word) < 0))); j--)
+				{
+					phraseTop10[j + 1].word1 = phraseTop10[j].word1;
+					phraseTop10[j + 1].word2 = phraseTop10[j].word2;
+					phraseTop10[j + 1].time = phraseTop10[j].time;
+				}
+				if (j < 9)
+				{
+					phraseTop10[j + 1].word1 = q->word1;
+					phraseTop10[j + 1].word2 = q->word2;
+					phraseTop10[j + 1].time = q->time;
+				}
+				q = q->next;
+			}
+		}
+	}
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%s %s: %d\n", phraseTop10[i].word1->word, phraseTop10[i].word2->word, phraseTop10[i].time);
+	}
 
 	getchar();
 	return 0;
